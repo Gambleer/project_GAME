aee79452f70dcd66641113d235cf944ee70708c8
Gambleer <rkostun@outlook.com>
2017-01-18 22:15:26 +0100

    TABLICA WYNIKOW - ogarniete save'y

--- Project GAME/Program.cs
+++ Project GAME/Program.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Media;
 using System.Windows.Input;
+using System.IO;
 
 namespace THE_GAME
 {
@@ -122,6 +123,86 @@ namespace THE_GAME
 
             Console.ReadKey(true);
             
+        }
+    }
+
+    public class tablica_wynikow
+ 
+    {
+
+        public static int odczytaj_wartosc(int numer_save)
+        {
+            string nazwaPliku = @"save\minigame1.txt";
+            FileStream plik1 = new FileStream(nazwaPliku, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            System.IO.StreamReader odczyt = new StreamReader(plik1);
+
+            int s = Convert.ToInt32(odczyt.ReadLine());
+
+            odczyt.Close();
+            plik1.Close();
+
+            return s;
+
+        }
+
+        public static void wpisz_wynik(int numer_pliku, int wartosc)
+
+        {
+
+            if (numer_pliku == 1)
+
+            {
+                string nazwaPliku = @"save\minigame1.txt";
+
+                int temp = odczytaj_wartosc(1);
+
+                if (temp < wartosc)
+                {
+                    FileStream plik1 = new FileStream(nazwaPliku, FileMode.Create, FileAccess.Write, FileShare.None);
+                    System.IO.StreamWriter zapis = new StreamWriter(plik1);
+
+                    zapis.Write(wartosc);
+                    zapis.Close();
+                    plik1.Close();
+                }
+            }
+
+            if (numer_pliku == 2)
+
+            {
+                string nazwaPliku = @"save\minigame1.txt";
+
+                int temp = odczytaj_wartosc(2);
+
+                if (temp < wartosc)
+                {
+                    FileStream plik2 = new FileStream(nazwaPliku, FileMode.Create, FileAccess.Write, FileShare.None);
+                    System.IO.StreamWriter zapis = new StreamWriter(plik2);
+
+                    zapis.Write(wartosc);
+                    zapis.Close();
+                    plik2.Close();
+                }
+            }
+
+            if (numer_pliku == 3)
+
+            {
+                string nazwaPliku = @"save\minigame1.txt";
+
+                int temp = odczytaj_wartosc(3);
+
+                if (temp < wartosc)
+                {
+                    FileStream plik3 = new FileStream(nazwaPliku, FileMode.Create, FileAccess.Write, FileShare.None);
+                    System.IO.StreamWriter zapis = new StreamWriter(plik3);
+
+                    zapis.Write(wartosc);
+                    zapis.Close();
+                    plik3.Close();
+                }
+            }
+
         }
     }
 
@@ -356,6 +437,8 @@ namespace THE_GAME
             Console.WriteLine("Kliknij klawisz aby zacząć!");
 
             start_gry();
+
+            tablica_wynikow.wpisz_wynik(1, 11);
 
             Console.ReadKey(true);
 
